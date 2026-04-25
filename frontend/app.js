634ce238b95d31091b91e55f07464e75dee5e064
sofnarub <sofnarub@gmail.com>
2026-04-25 15:38:39 +0200

    arreglado js

--- frontend/app.js
+++ frontend/app.js
@@ -10,7 +10,7 @@ document.getElementById("formulario").addEventListener("submit", async function(
     resultado.textContent = "Entrada inválida";
     return;
   }
-x = ?;
+
   try {
     const response = await fetch("/backend/procesar.php", {
       method: "POST",
